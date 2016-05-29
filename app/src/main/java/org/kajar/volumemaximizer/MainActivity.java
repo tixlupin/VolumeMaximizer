7be0a5433b7984ae538754c8a68593deb43546a5
lupin <others@kajar.org>
2016-05-29 21:39:33 +0900

    first commit

---
+++ app/src/main/java/org/kajar/volumemaximizer/MainActivity.java
@@ -0,0 +1,47 @@
+package org.kajar.volumemaximizer;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // no activity
+        //setContentView(R.layout.activity_main);
+
+        AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        am.setParameters("av_volume=100");
+
+        Toast.makeText(this, "Setting volume completed", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
